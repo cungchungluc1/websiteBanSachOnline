4e3d16d6c7d795e5a331e2f878c155b650880cc5
huynguyen0mk1 <50603994+huynguyen0mk1@users.noreply.github.com>
2022-01-01 16:31:37 +0700

    1

--- pages/controller/login.php
+++ pages/controller/login.php
@@ -9,24 +9,20 @@
 
 <body>
     <?php
-
-require_once '../src/Facebook/autoload.php';
-session_start();
-
-$fb = new Facebook\Facebook([
-  'app_id' => '393692505877281',
-  'app_secret' => '76731419c560906c89e49ecee89f0dca',
-  'default_graph_version' => 'v2.8',
-  'allowSignedRequest' => false 
-  ]);
-
-$helper = $fb->getRedirectLoginHelper();
-$permissions = array(
-  'email'
-);
-$loginUrl = $helper->getLoginUrl('http://localhost:18080/websitebansachonline/pages/controller/fb-callback.php', $permissions);
-
-echo '<script>window.location.replace("'.$loginUrl.'")</script>';
+        require_once '../src/Facebook/autoload.php';
+        session_start();
+        $fb = new Facebook\Facebook([
+          'app_id' => '393692505877281',
+          'app_secret' => '76731419c560906c89e49ecee89f0dca',
+          'default_graph_version' => 'v2.8',
+          'allowSignedRequest' => false 
+          ]);
+        $helper = $fb->getRedirectLoginHelper();
+        $permissions = array(
+          'email'
+        );
+        $loginUrl = $helper->getLoginUrl('http://localhost:18080/websitebansachonline/pages/controller/fb-callback.php', $permissions);
+        echo '<script>window.location.replace("'.$loginUrl.'")</script>';
 ?>
 </body>
 
